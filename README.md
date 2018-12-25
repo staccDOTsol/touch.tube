ee23bfdf652a118d42634512a68258d371ce47fe
Jarett Dunn <jarettrsdunn@gmail.com>
2018-12-25 14:32:01 +0000

    first comment

---
+++ README.md
@@ -0,0 +1,2 @@
+# inAppBrowserExample
+An example on how to inAppBrowser cordova plugin to an Ionic project
---
+++ src/pages/home/home.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { Platform } from 'ionic-angular';
+
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, private iab: InAppBrowser,platform: Platform) {
+platform.ready().then(() => {
+
+const browser = this.iab.create('https://touch.tube', '_self');
+
+   browser.show();
+})
+  
+}
+}
